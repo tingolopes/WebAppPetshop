2446af61bd02fda7c7b8abf8499dee73dc3ff4b3
tingo <tingolopes@gmail.com>
2019-05-29 21:02:43 -0400

    Forma de pagamento

--- src/java/br/com/petshop/model/FormaDePagamento.java
+++ src/java/br/com/petshop/model/FormaDePagamento.java
@@ -1,4 +1,7 @@
 package br.com.petshop.model;
-public enum FormaDePagamento {
-    
+
+import java.io.Serializable;
+
+public enum FormaDePagamento implements Serializable {
+    DINHEIRO, CARTAO_CREDITO, CARTAO_DEBITO, CHEQUE, BOLETO_BANCARIO, DEPOSITO_BANCARIO;
 }
