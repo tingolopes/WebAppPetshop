518880da99ee4285ae15f69d362ee068e493d93b
Kleber <kleber.forsten@gmail.com>
2019-06-02 21:06:14 -0400

    WS animais e racas

--- src/java/br/com/petshop/webservices/Petshop.java
+++ src/java/br/com/petshop/webservices/Petshop.java
@@ -6,7 +6,9 @@
 package br.com.petshop.webservices;
 
 import br.com.petshop.dao.DAO;
+import br.com.petshop.model.Animal;
 import br.com.petshop.model.Cliente;
+import br.com.petshop.model.Raca;
 import com.google.gson.Gson;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.UriInfo;
@@ -57,6 +59,22 @@ public class Petshop {
     
     @GET
     @Produces(MediaType.APPLICATION_JSON)
+    @Path("animal/list")
+    public String listAnimais() {
+        Gson g = new Gson();
+        return g.toJson(new DAO<>(Animal.class).listaTodos());
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("raca/list")
+    public String listRacas() {
+        Gson g = new Gson();
+        return g.toJson(new DAO<>(Raca.class).listaTodos());
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
     @Path("cliente/get/{id}")
     public String getCliente(@PathParam("id") Integer id) {
         Cliente c = new Cliente();
@@ -69,24 +87,92 @@ public class Petshop {
         return g.toJson(c);
     }
     
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("animal/get/{id}")
+    public String getAnimal(@PathParam("id") Integer id) {
+        Animal a = new Animal();
+        a.setId(id);
+        
+        DAO<Animal> dao = new DAO<>(Animal.class);
+        a = dao.porId(id);
+        
+        Gson g = new Gson();
+        return g.toJson(a);
+    }
+    
+    @GET
+    @Produces(MediaType.APPLICATION_JSON)
+    @Path("raca/get/{id}")
+    public String getRaca(@PathParam("id") Integer id) {
+        Raca r = new Raca();
+        r.setId(id);
+        
+        DAO<Raca> dao = new DAO<>(Raca.class);
+        r = dao.porId(id);
+        
+        Gson g = new Gson();
+        return g.toJson(r);
+    }
+    
     
     @GET
     //@Produces(MediaType.APPLICATION_JSON)
     @Path("cliente/excluir/{id}")
-    public String excluir(@PathParam("id") Integer id) {
+    public String excluirCliente(@PathParam("id") Integer id) {
         Cliente c = new Cliente();
         c.setId(id);
         new DAO<>(Cliente.class).excluir(c);
         return "Registro excluído com sucesso!";
     }
     
+    @GET
+    //@Produces(MediaType.APPLICATION_JSON)
+    @Path("animal/excluir/{id}")
+    public String excluirAnimal(@PathParam("id") Integer id) {
+        Animal a = new Animal();
+        a.setId(id);
+        new DAO<>(Animal.class).excluir(a);
+        return "Registro excluído com sucesso!";
+    }
+    
+    @GET
+    //@Produces(MediaType.APPLICATION_JSON)
+    @Path("raca/excluir/{id}")
+    public String excluirRaca(@PathParam("id") Integer id) {
+        Raca r = new Raca();
+        r.setId(id);
+        new DAO<>(Raca.class).excluir(r);
+        return "Registro excluído com sucesso!";
+    }
+    
     @POST
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("cliente/inserir")
-    public String inserir(String content){
+    public String inserirCliente(String content){
         Gson g = new Gson();
         Cliente c = (Cliente) g.fromJson(content, Cliente.class);
         new DAO<>(Cliente.class).salvar(c);
+        return "Registro salvo com sucesso!";
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("animal/inserir")
+    public String inserirAnimal(String content){
+        Gson g = new Gson();
+        Animal a = (Animal) g.fromJson(content, Animal.class);
+        new DAO<>(Animal.class).salvar(a);
+        return "Registro salvo com sucesso!";
+    }
+    
+    @POST
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("raca/inserir")
+    public String inserirRaca(String content){
+        Gson g = new Gson();
+        Raca r = (Raca) g.fromJson(content, Raca.class);
+        new DAO<>(Raca.class).salvar(r);
         return "Registro salvo com sucesso!";
     }
     
@@ -97,10 +183,28 @@ public class Petshop {
     @PUT
     @Consumes(MediaType.APPLICATION_JSON)
     @Path("cliente/alterar")
-    public void alterar(String content) {
+    public void alterarCliente(String content) {
         Gson g = new Gson();
         Cliente c = (Cliente) g.fromJson(content, Cliente.class);
         new DAO<>(Cliente.class).alterar(c);
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("animal/alterar")
+    public void alterarAnimal(String content) {
+        Gson g = new Gson();
+        Animal a = (Animal) g.fromJson(content, Animal.class);
+        new DAO<>(Animal.class).alterar(a);
+    }
+    
+    @PUT
+    @Consumes(MediaType.APPLICATION_JSON)
+    @Path("raca/alterar")
+    public void alterarRaca(String content) {
+        Gson g = new Gson();
+        Raca r = (Raca) g.fromJson(content, Raca.class);
+        new DAO<>(Raca.class).alterar(r);
     }
     
     @PUT
