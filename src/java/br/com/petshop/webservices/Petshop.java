f69d58cc0da4dd7ea08948e6c6e6bd1a3605a04d
tingo <tingolopes@gmail.com>
2019-06-17 21:36:00 -0400

    Inclusão do charset=utf-8 na API

--- src/java/br/com/petshop/webservices/Petshop.java
+++ src/java/br/com/petshop/webservices/Petshop.java
@@ -19,7 +19,6 @@ import javax.ws.rs.POST;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
 import javax.ws.rs.PathParam;
-import javax.ws.rs.core.MediaType;
 
 /**
  * REST Web Service
@@ -39,85 +38,85 @@ public class Petshop {
     }
 
     /**
-     * Retrieves representation of an instance of br.com.petshop.webservices.Petshop
+     * Retrieves representation of an instance of
+     * br.com.petshop.webservices.Petshop
+     *
      * @return an instance of java.lang.String
      */
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     public String getJson() {
         //TODO return proper representation object
         throw new UnsupportedOperationException();
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("cliente/list")
     public String listClientes() {
         Gson g = new Gson();
         return g.toJson(new DAO<>(Cliente.class).listaTodos());
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("animal")
     public String listAnimais() {
         Gson g = new Gson();
         return g.toJson(new DAO<>(Animal.class).listaTodos());
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("raca/list")
     public String listRacas() {
         Gson g = new Gson();
         return g.toJson(new DAO<>(Raca.class).listaTodos());
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("cliente/get/{id}")
     public String getCliente(@PathParam("id") Integer id) {
         Cliente c = new Cliente();
         c.setId(id);
-        
+
         DAO<Cliente> dao = new DAO<>(Cliente.class);
         c = dao.porId(id);
-        
+
         Gson g = new Gson();
         return g.toJson(c);
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("animal/{id}")
     public String getAnimal(@PathParam("id") Integer id) {
         Animal a = new Animal();
         a.setId(id);
-        
+
         DAO<Animal> dao = new DAO<>(Animal.class);
         a = dao.porId(id);
-        
+
         Gson g = new Gson();
         return g.toJson(a);
     }
-    
+
     @GET
-    @Produces(MediaType.APPLICATION_JSON)
+    @Produces("application/json; charset=UTF-8")
     @Path("raca/get/{id}")
     public String getRaca(@PathParam("id") Integer id) {
         Raca r = new Raca();
         r.setId(id);
-        
+
         DAO<Raca> dao = new DAO<>(Raca.class);
         r = dao.porId(id);
-        
+
         Gson g = new Gson();
         return g.toJson(r);
     }
-    
-    
+
     @GET
-    //@Produces(MediaType.APPLICATION_JSON)
     @Path("cliente/excluir/{id}")
     public String excluirCliente(@PathParam("id") Integer id) {
         Cliente c = new Cliente();
@@ -125,9 +124,8 @@ public class Petshop {
         new DAO<>(Cliente.class).excluir(c);
         return "Registro excluído com sucesso!";
     }
-    
+
     @GET
-    //@Produces(MediaType.APPLICATION_JSON)
     @Path("animal/excluir/{id}")
     public String excluirAnimal(@PathParam("id") Integer id) {
         Animal a = new Animal();
@@ -135,9 +133,8 @@ public class Petshop {
         new DAO<>(Animal.class).excluir(a);
         return "Registro excluído com sucesso!";
     }
-    
+
     @GET
-    //@Produces(MediaType.APPLICATION_JSON)
     @Path("raca/excluir/{id}")
     public String excluirRaca(@PathParam("id") Integer id) {
         Raca r = new Raca();
@@ -145,73 +142,73 @@ public class Petshop {
         new DAO<>(Raca.class).excluir(r);
         return "Registro excluído com sucesso!";
     }
-    
+
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("cliente/inserir")
-    public String inserirCliente(String content){
+    public String inserirCliente(String content) {
         Gson g = new Gson();
         Cliente c = (Cliente) g.fromJson(content, Cliente.class);
         new DAO<>(Cliente.class).salvar(c);
         return "Registro salvo com sucesso!";
     }
-    
+
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("animal")
-    public String inserirAnimal(String content){
+    public String inserirAnimal(String content) {
         Gson g = new Gson();
         Animal a = (Animal) g.fromJson(content, Animal.class);
         a.setId(null);
         new DAO<>(Animal.class).salvar(a);
         return "Registro salvo com sucesso!";
     }
-    
+
     @POST
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("raca/inserir")
-    public String inserirRaca(String content){
+    public String inserirRaca(String content) {
         Gson g = new Gson();
         Raca r = (Raca) g.fromJson(content, Raca.class);
         new DAO<>(Raca.class).salvar(r);
         return "Registro salvo com sucesso!";
     }
-    
+
     /**
      * PUT method for updating or creating an instance of Petshop
+     *
      * @param content representation for the resource
      */
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("cliente/alterar")
     public void alterarCliente(String content) {
         Gson g = new Gson();
         Cliente c = (Cliente) g.fromJson(content, Cliente.class);
         new DAO<>(Cliente.class).alterar(c);
     }
-    
+
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("animal")
     public void alterarAnimal(String content) {
         Gson g = new Gson();
         Animal a = (Animal) g.fromJson(content, Animal.class);
         new DAO<>(Animal.class).alterar(a);
     }
-    
+
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     @Path("raca/alterar")
     public void alterarRaca(String content) {
         Gson g = new Gson();
         Raca r = (Raca) g.fromJson(content, Raca.class);
         new DAO<>(Raca.class).alterar(r);
     }
-    
+
     @PUT
-    @Consumes(MediaType.APPLICATION_JSON)
+    @Consumes("application/json; charset=UTF-8")
     public void putJson(String content) {
     }
-    
-    
+
 }
